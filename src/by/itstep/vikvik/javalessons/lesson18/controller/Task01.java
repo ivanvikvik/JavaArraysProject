d9b5c4c5d5459e17650cb72a7d2c83a5f6c09bf4
Victor <ivanvikvik@gmail.com>
2022-06-16 19:58:41 +0300

    Add Task01 Solution

---
+++ src/by/itstep/vikvik/javalessons/lesson18/controller/Task01.java
@@ -0,0 +1,24 @@
+package by.itstep.vikvik.javalessons.lesson18.controller;
+
+import by.itstep.vikvik.javalessons.lesson18.view.Printer;
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size;
+
+        do {
+            Printer.print("Input size of array: ");
+            size = scanner.nextInt();
+        } while(size <= 0);
+
+        int[] array = new int[size];
+
+        // logic
+
+        //output result
+
+    }
+}
