b93c63aafa78af49ee301a6b015d64e23e01e02f
Victor <ivanvikvik@gmail.com>
2022-06-21 21:41:29 +0300

    Add bubble sorting algorithm (v. 1.0)

---
+++ src/by/itstep/vikvik/javalessons/lesson18_19/controller/Task05.java
@@ -0,0 +1,36 @@
+package by.itstep.vikvik.javalessons.lesson18_19.controller;
+
+import by.itstep.vikvik.javalessons.lesson18_19.model.logic.ArraySorter;
+import by.itstep.vikvik.javalessons.lesson18_19.model.logic.ArrayWorker;
+import by.itstep.vikvik.javalessons.lesson18_19.util.ArrayInitializer;
+import by.itstep.vikvik.javalessons.lesson18_19.util.Convertor;
+import by.itstep.vikvik.javalessons.lesson18_19.view.Printer;
+
+import java.util.Scanner;
+
+public class Task05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size;
+
+        do {
+            Printer.print("Input size of array: ");
+            size = scanner.nextInt();
+        } while (size <= 0);
+
+        int[] array = new int[size];
+
+        ArrayInitializer.randomInit(array);
+
+        Printer.print("Array before sort: " + Convertor.toString(array));
+
+        ArraySorter.bubbleSortAcs(array);
+
+        Printer.print("\nArray after sort: " + Convertor.toString(array));
+
+        ArrayWorker.reverse(array);
+
+        Printer.print("\nArray after reverse: " + Convertor.toString(array));
+
+    }
+}
--- src/by/itstep/vikvik/javalessons/lesson18_19/model/logic/ArraySorter.java
+++ src/by/itstep/vikvik/javalessons/lesson18_19/model/logic/ArraySorter.java
@@ -2,7 +2,15 @@ package by.itstep.vikvik.javalessons.lesson18_19.model.logic;
 
 public class ArraySorter {
     public static void bubbleSortAcs(int[] array) {
-
+        for (int j = 0; j < array.length - 1; j++) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] < array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+        }
     }
 
     public static void selectedSortAcs(int[] array) {
